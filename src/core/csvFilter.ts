8d58e00882f96f25f542e3398ed8c30610afeb42
Luis <luis.amez.vitoria@gmail.com>
2026-01-26 16:27:40 +0100

    Add functionality and tests for filtering invoices with repeated invoice number

--- src/core/csvFilter.ts
+++ src/core/csvFilter.ts
@@ -1,1 +1,85 @@
-export const csvFilter = (input: string) => input;
+export class CsvInvoiceFilter {
+  private static invoiceHeaderFields = {
+    numFactura: 'Num_factura',
+    fecha: 'Fecha',
+    bruto: 'Bruto',
+    neto: 'Neto',
+    iva: 'IVA',
+    igic: 'IGIC',
+    concepto: 'Concepto',
+    cifCliente: 'CIF_cliente',
+    nifCliente: 'NIF_cliente',
+  };
+  private static invoiceHeaderAsArray = [
+    this.invoiceHeaderFields.numFactura,
+    this.invoiceHeaderFields.fecha,
+    this.invoiceHeaderFields.bruto,
+    this.invoiceHeaderFields.neto,
+    this.invoiceHeaderFields.iva,
+    this.invoiceHeaderFields.igic,
+    this.invoiceHeaderFields.concepto,
+    this.invoiceHeaderFields.cifCliente,
+    this.invoiceHeaderFields.nifCliente,
+  ];
+
+  private csvFile: string;
+  private invoices: string[] = [];
+
+  constructor(csvFile: string) {
+    this.csvFile = csvFile;
+  }
+
+  filterInvoices() {
+    const linesSeparator = '\n';
+    const lines = this.csvFile.split(linesSeparator);
+    const headerLine = 0;
+    const header = lines[headerLine];
+    const startLineForInvoices = 1;
+    this.invoices = lines.slice(startLineForInvoices);
+
+    if (header !== CsvInvoiceFilter.getInvoiceHeader()) throw new TypeError('Invalid Header');
+
+    return [header].concat(this.filterRepeatedInvoiceNumber().getFilteredInvoices()).join('\n');
+  }
+
+  private filterRepeatedInvoiceNumber() {
+    const invoiceNumbersFrequency = this.getMapOfInvoiceNumbers();
+
+    this.invoices = this.invoices.filter((invoice) => {
+      const invoiceNumber =
+        invoice.split(',')[
+          CsvInvoiceFilter.invoiceHeaderAsArray.indexOf(CsvInvoiceFilter.invoiceHeaderFields.numFactura)
+        ];
+      const frequency = invoiceNumbersFrequency.get(invoiceNumber);
+      return frequency && frequency === 1;
+    });
+
+    return this;
+  }
+
+  private getMapOfInvoiceNumbers() {
+    const invoiceNumbers = this.invoices.map(
+      (invoice) =>
+        invoice.split(',')[
+          CsvInvoiceFilter.invoiceHeaderAsArray.indexOf(CsvInvoiceFilter.invoiceHeaderFields.numFactura)
+        ]
+    );
+
+    const invoiceNumbersFrequency = new Map<string, number>();
+    for (const invoiceNumber of invoiceNumbers) {
+      const frequency = invoiceNumbersFrequency.get(invoiceNumber);
+      if (frequency) invoiceNumbersFrequency.set(invoiceNumber, frequency + 1);
+      else invoiceNumbersFrequency.set(invoiceNumber, 1);
+    }
+
+    return invoiceNumbersFrequency;
+  }
+
+  private getFilteredInvoices() {
+    return this.invoices;
+  }
+
+  static getInvoiceHeader() {
+    return this.invoiceHeaderAsArray.join(',');
+  }
+}
--- src/tests/csvFilter.test.ts
+++ src/tests/csvFilter.test.ts
@@ -1,7 +1,42 @@
-import { csvFilter } from '../core/csvFilter';
+import { CsvInvoiceFilter } from '../core/csvFilter';
 
 describe('csvFilter', () => {
-  it('runs', () => {
-    expect(csvFilter('hello, world')).toEqual('hello, world');
+  it('fails if the file does not have the right header', () => {
+    expect(() => {
+      new CsvInvoiceFilter('hello, world').filterInvoices();
+    }).toThrow('Invalid Header');
+  });
+
+  it('filters nothing if the file only has the right header', () => {
+    const csvInvoiceFilter = new CsvInvoiceFilter(CsvInvoiceFilter.getInvoiceHeader());
+
+    const result = csvInvoiceFilter.filterInvoices();
+
+    expect(result).toBe(CsvInvoiceFilter.getInvoiceHeader());
+  });
+
+  it('filters nothing if the file has the right information', () => {
+    const invoices =
+      CsvInvoiceFilter.getInvoiceHeader() +
+      '\n1,02/05/2019,960,800,20,,ACERLaptop,B76430134,' +
+      '\n2,03/08/2019,2160,2000,,8,MacBook Pro,,78544372A';
+    const csvInvoiceFilter = new CsvInvoiceFilter(invoices);
+
+    const result = csvInvoiceFilter.filterInvoices();
+
+    expect(result).toBe(invoices);
+  });
+
+  it('filters out all the invoices with repeated invoice number', () => {
+    const distinctNumberInvoices =
+      CsvInvoiceFilter.getInvoiceHeader() + '\n1,02/05/2019,960,800,20,,ACERLaptop,B76430134,';
+    const repeatedNumberInvoices =
+      '\n2,03/08/2019,2160,2000,,8,MacBook Pro,,78544372A' + '\n2,10/11/2019,2200,2000,,10,MacBook Pro,,78544372A';
+    const allInvoices = distinctNumberInvoices + repeatedNumberInvoices;
+    const csvInvoiceFilter = new CsvInvoiceFilter(allInvoices);
+
+    const result = csvInvoiceFilter.filterInvoices();
+
+    expect(result).toBe(distinctNumberInvoices);
   });
 });
